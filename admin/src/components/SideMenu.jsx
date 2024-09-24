e937b9c0f36f7c4b4b97bf678e2c801bec6b7279
venkatapavan22 <pavanvvp123@gmail.com>
2024-09-24 21:21:15 +0530

    added navlinks

--- admin/src/components/SideMenu.jsx
+++ admin/src/components/SideMenu.jsx
@@ -5,7 +5,7 @@ const SideMenu = () => {
   return (
     <div className='w-[18%] min-h-screen border-r-2'>
       <div className='flex flex-col gap-4 pt-6 pl-[20%] text-[15px]'>
-
+        {/* {------navlinks-----------} */}
         <NavLink to="/add" className="flex items-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-l">
             <img className='w-5 h-5' src={assets.add_icon} alt="" />
             <p className='hidden md:block'>add Items</p>
